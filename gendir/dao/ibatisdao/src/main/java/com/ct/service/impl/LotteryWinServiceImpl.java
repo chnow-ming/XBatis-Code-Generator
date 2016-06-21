b52fd11ef0398c224d1228235eae630cb2e3aca3
chnow <361726442@qq.com>
2016-06-21 18:11:22 +0800

    修复bug

--- gendir/dao/ibatisdao/src/main/java/com/ct/service/impl/LotteryWinServiceImpl.java
+++ gendir/dao/ibatisdao/src/main/java/com/ct/service/impl/LotteryWinServiceImpl.java
@@ -3,7 +3,7 @@ package com.ct.service.impl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import com.ct.pojo.LotteryWin;
 import com.ct.dto.LotteryWinQuery;
 import com.ct.Base.service.impl.BaseServiceImpl;
@@ -12,8 +12,8 @@ import com.ct.service.LotteryWinService;
  * @author Wu shaoming(361726442@qq.com)
  * @since 2016-06-21
  */
-
+@Component
 public class LotteryWinServiceImpl extends BaseServiceImpl<LotteryWin, LotteryWinQuery, Long> implements LotteryWinService{
 	
-
+private static final Log log = LogFactory.getLog(LotteryWinServiceImpl.class);
 }
